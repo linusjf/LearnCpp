6dff0d94b7fadbbde812b3789ba3e897ee3b9fb0
fernal73 <linus.fernandes@gmail.com>
2021-06-10 05:26:25 +0530

    Add Scope.cpp

---
+++ C++/Scope.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int g;
+int t;
+
+int main()
+{
+ // Local variable declaration:
+ int a, b;
+ int c;
+ int t = 30;
+// actual initialization
+ a = 10;
+ b = 20;
+ c = a + b;
+ cout << c << endl;
+ g = a + b;
+ cout << g << endl;
+ cout << t << endl;
+ return 0;
+}
