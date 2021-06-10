332fdb4091591f7e5e2e8d4a95129e8a7d292b01
fernal73 <linus.fernandes@gmail.com>
2021-06-10 08:31:03 +0530

    Add EscapeChars

---
+++ C++/EscapeChars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "\\" << endl;
+ cout << "\"" << endl;
+ cout << "\'" << endl;
+ cout << "\?" << endl;
+ cout << "\a" << endl;
+ cout << "\b" << endl;
+ cout << "\f" << endl;
+ cout << "\216" << endl;
+ cout << "\x21\x64" << endl;
+ cout << "Hello,\tWorld\n\r\n\r";
+ cout << "Hello,\vWorld\n\r\n\r";
+ return 0;
+}
