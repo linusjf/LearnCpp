c702ff5772a6fdfb46cce74f34b555e69a88dec3
fernal73 <linus.fernandes@gmail.com>
2021-06-11 11:40:31 +0530

    refactor

--- C++/structs.cpp
+++ C++/structs.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-struct Books {
+void printBook(struct Book book);
+
+struct Book {
   char title[50];
   char author[50];
   char subject[100];
@@ -12,9 +14,9 @@ struct Books {
 
 int main() {
   // Declare Book1 of type Book
-  struct Books Book1;
+  struct Book Book1;
   // Declare Book2 of type Book
-  struct Books Book2;
+  struct Book Book2;
   // book 1 specification
   strcpy(Book1.title, "Learn C++ Programming");
   strcpy(Book1.author, "Chand Miyan");
@@ -26,14 +28,15 @@ int main() {
   strcpy(Book2.subject, "Telecom");
   Book2.book_id = 6495700;
   // Print Book1 info
-  cout << "Book 1 title: " << Book1.title << endl;
-  cout << "Book 1 author: " << Book1.author << endl;
-  cout << "Book 1 subject: " << Book1.subject << endl;
-  cout << "Book 1 id: " << Book1.book_id << endl;
+  printBook(Book1);
   // Print Book2 info
-  cout << "Book 2 title: " << Book2.title << endl;
-  cout << "Book 2 author: " << Book2.author << endl;
-  cout << "Book 2 subject: " << Book2.subject << endl;
-  cout << "Book 2 id: " << Book2.book_id << endl;
+  printBook(Book2);
   return 0;
+}
+
+void printBook(struct Book book) {
+  cout << "Book title: " << book.title << endl;
+  cout << "Book author: " << book.author << endl;
+  cout << "Book subject: " << book.subject << endl;
+  cout << "Book id: " << book.book_id << endl;
 }
