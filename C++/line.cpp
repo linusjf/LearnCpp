c18f9586d5e772d8f86d0c2725a1a43d5446bc7f
fernal73 <linus.fernandes@gmail.com>
2021-06-12 10:10:47 +0530

    Add copy constructor

--- C++/line.cpp
+++ C++/line.cpp
@@ -6,36 +6,51 @@ class Line {
  public:
   Line();
   Line(double len);
+  Line(const Line& line);
   ~Line();
   void setLength(double len);
   double getLength(void);
 
  protected:
-  double length;
+  double* length;
 };
 
-Line::Line(void) {
-  length = 0;
+Line::Line(void) : Line(0.0) {
 }
 
-Line::Line(double len) : length(len) {
+Line::Line(double len) {
+  length = new double;
+  *length = len;
+}
+
+Line::Line(const Line& obj) : Line(*obj.length) {
 }
 
 Line::~Line() {
+  delete length;
 }
 
 double Line::getLength(void) {
-  return length;
+  return *length;
 }
 
 void Line::setLength(double len) {
-  length = len;
+  *length = len;
+}
+
+void display(Line obj) {
+  cout << "Length of line : " << obj.getLength() << endl;
 }
 
 int main() {
   Line line;
   // set line length
   line.setLength(6.0);
-  cout << "Length of line : " << line.getLength() << endl;
+  display(line);
+  Line line2(10);
+  display(line2);
+  // This also calls copy constructor
+  Line line3 = line;
+  display(line3);
   return 0;
 }
