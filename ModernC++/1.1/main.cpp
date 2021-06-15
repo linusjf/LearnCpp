44659ab21b5babbdeccd81e106756deb0cd8308c
fernal73 <linus.fernandes@gmail.com>
2021-06-15 20:28:26 +0530

    Start Modern c++ tutorial

---
+++ ModernC++/1.1/main.cpp
@@ -0,0 +1,10 @@
+#include <functional>
+#include <iostream>
+#include "foo.h"
+
+int main() {
+  [out = std::ref(std::cout << "Result from C code: " << add(1, 2))]() {
+    out.get() << ".\n";
+  }();
+  return 0;
+}
