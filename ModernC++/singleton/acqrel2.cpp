e5308690d8d40ec00b32d7e82ca33fa0102bac61
fernal73 <linus.fernandes@gmail.com>
2021-06-20 14:16:52 +0530

    Add acqrel examples

---
+++ ModernC++/singleton/acqrel2.cpp
@@ -0,0 +1,56 @@
+#include <atomic>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+constexpr auto tenMill = 10000000;
+
+class MySingleton {
+ public:
+  static MySingleton* getInstance() {
+    MySingleton* sin = instance.load(memory_order_acquire);
+    if (!sin) {
+      lock_guard<mutex> myLock(myMutex);
+      sin = instance.load(memory_order_relaxed);
+      if (!sin) {
+        sin = new MySingleton();
+        instance.store(sin, memory_order_release);
+      }
+    }
+    // volatile int dummy{};
+    return sin;
+  }
+
+ private:
+  MySingleton() = default;
+  ~MySingleton() = default;
+  MySingleton(const MySingleton&) = delete;
+  MySingleton& operator=(const MySingleton&) = delete;
+
+  static atomic<MySingleton*> instance;
+  static mutex myMutex;
+};
+
+atomic<MySingleton*> MySingleton::instance;
+mutex MySingleton::myMutex;
+
+chrono::duration<double> getTime() {
+
+  auto begin = chrono::system_clock::now();
+  for (size_t i = 0; i <= tenMill; ++i) {
+    MySingleton::getInstance();
+  }
+  return chrono::system_clock::now() - begin;
+};
+
+int main() {
+  auto fut1 = async(launch::async, getTime);
+  auto fut2 = async(launch::async, getTime);
+  auto fut3 = async(launch::async, getTime);
+  auto fut4 = async(launch::async, getTime);
+  auto total = fut1.get() + fut2.get() + fut3.get() + fut4.get();
+  cout << total.count() << endl;
+}
