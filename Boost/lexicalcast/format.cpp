aba5de5bccf233179d530e3ed777234dd3ad46d6
fernal73 <linus.fernandes@gmail.com>
2021-06-24 21:08:33 +0530

    Add format examples

--- Boost/lexicalcast/format.cpp
+++ Boost/lexicalcast/format.cpp
@@ -1,8 +1,34 @@
 #include <boost/format.hpp>
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct animal {
+  string name;
+  int legs;
+};
+
+ostream &operator<<(ostream &os, const animal &a) {
+  return os << a.name << ',' << a.legs;
+}
+
 int main() {
-  cout << boost::format{"%1%.%2%.%3%"} % 12 % 5 % 2014 << '\n';
-  cout << boost::format{"%2%.%1%.%3%"} % 12 % 5 % 2014 << '\n';
+  cout << boost::format{"%1%.%2%.%3%"} % 12 % 5 % 2014 << endl;
+  cout << boost::format{"%2%.%1%.%3%"} % 12 % 5 % 2014 << endl;
+  cout << boost::format{"%1% %2% %1%"} % boost::io::group(showpos, 1) % 2
+       << endl;
+  cout << boost::format{"%|1$+| %2% %1%"} % 1 % 2 << endl;
+  cout << boost::format{"%+d %d %d"} % 1 % 2 % 1 << endl;
+  cout << boost::format{"%|+| %|| %||"} % 1 % 2 % 1 << endl;
+  cout << boost::format{"%+s %s %s"} % 1 % 2 % 1 << endl;
+  animal a{"cat", 4};
+  cout << boost::format{"%1%"} % a << endl;
+  try
+  {
+    cout << boost::format{"%|+| %2% %1%"} % 1 % 2 << endl;
+  }
+  catch (boost::io::format_error &ex)
+  {
+    cout << ex.what() << endl;
+  }
 }
