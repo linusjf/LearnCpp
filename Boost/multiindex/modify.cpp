32acc8bf19578fbb62d42c8e23e872c8bffea038
fernal73 <linus.fernandes@gmail.com>
2021-06-26 17:35:02 +0530

    Add multiindex examples

---
+++ Boost/multiindex/modify.cpp
@@ -0,0 +1,33 @@
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index_container.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace boost::multi_index;
+
+struct animal {
+  string name;
+  int legs;
+};
+
+typedef multi_index_container<
+    animal,
+    indexed_by<hashed_non_unique<member<animal, string, &animal::name> >,
+               hashed_non_unique<member<animal, int, &animal::legs> > > >
+    animal_multi;
+
+int main() {
+  animal_multi animals;
+
+  animals.insert({"cat", 4});
+  animals.insert({"shark", 0});
+  animals.insert({"spider", 8});
+
+  auto &legs_index = animals.get<1>();
+  auto it = legs_index.find(4);
+  legs_index.modify(it, [](animal &a) { a.name = "dog"; });
+
+  cout << animals.count("dog") << endl;
+}
