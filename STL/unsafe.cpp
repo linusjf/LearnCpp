fbbacc17d82e886530f360bd7822ea842533bd8a
fernal73 <linus.fernandes@gmail.com>
2021-07-12 17:32:11 +0530

    add unsafe

--- STL/unsafe.cpp
+++ STL/unsafe.cpp
@@ -7,5 +7,10 @@ int main() {
   const size_t container_size{1000};
   vector<int> v(container_size, 123);
   cout << "Out of range element value: " << v[container_size + 10] << '\n';
-  cout << "Out of range element value: " << v.at(container_size + 10) << '\n';
+  try {
+    cout << "Out of range element value: " << v.at(container_size + 10)
+              << '\n';
+  } catch (const out_of_range &e) {
+    cout << "Ooops, out of range access detected: " << e.what() << '\n';
+  }
 }
