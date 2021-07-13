ed3edc6af573e18420b8f384944f2f10c1b67929
fernal73 <linus.fernandes@gmail.com>
2021-07-13 20:27:56 +0530

    Add iterator

---
+++ STL/iterator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class num_iterator {
+  int i;
+
+ public:
+  explicit num_iterator(int position = 0) : i{position} {
+  }
+  int operator*() const {
+    return i;
+  }
+  num_iterator& operator++() {
+    ++i;
+    return *this;
+  }
+  bool operator!=(const num_iterator& other) const {
+    return i != other.i;
+  }
+};
+
+class num_range {
+  int a;
+  int b;
+
+ public:
+  num_range(int from, int to) : a{from}, b{to} {
+  }
+  num_iterator begin() const {
+    return num_iterator{a};
+  }
+  num_iterator end() const {
+    return num_iterator{b};
+  }
+};
+
+int main() {
+  for (int i: num_range{100, 110}) 
+    cout << i << ", ";
+  cout << endl;
+}
