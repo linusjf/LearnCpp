c2af6082e8b9e771b96ba758a2f7398a71f9d86a
fernal73 <linus.fernandes@gmail.com>
2021-07-13 09:32:25 +0530

    Add more template examples

---
+++ STL/multiset.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+using namespace std;
+
+int main() {
+  multiset<string> s;
+  istream_iterator<string> it{cin};
+  istream_iterator<string> end;
+  copy(it, end, inserter(s, s.end()));
+  for (const auto &word: s)
+    cout << word << ", ";
+  cout << endl;
+}
---
+++ STL/unordered.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+struct coord {
+  int x;
+  int y;
+};
+
+bool operator==(const coord &l, const coord &r) {
+  return l.x == r.x && l.y == r.y;
+}
+
+namespace std {
+template <>
+struct hash<coord> {
+  using argument_type = coord;
+  using result_type = size_t;
+  result_type operator()(const argument_type &c) const {
+    return static_cast<result_type>(c.x) + static_cast<result_type>(c.y);
+  }
+};
+}  // namespace std
+
+int main() {
+  unordered_map<coord, int> m{{{0, 0}, 1}, {{0, 1}, 2}, {{2, 1}, 3}};
+  for (const auto &[key, value]: m) {
+    cout << "{(" << key.x << ", " << key.y << "): " << value << "} ";
+  }
+  cout << endl;
+}
