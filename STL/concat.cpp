3ea2fb773f436ac28ad27eee987a4df06538f104
fernal73 <linus.fernandes@gmail.com>
2021-07-15 10:55:24 +0530

    Add concat

---
+++ STL/concat.cpp
@@ -0,0 +1,29 @@
+#include <functional>
+#include <iostream>
+
+using namespace std;
+template <typename T, typename... Ts>
+auto concat(T t, Ts... ts) {
+  if constexpr (sizeof...(ts) > 0) {
+    return [=](auto... parameters) { return t(concat(ts...)(parameters...)); };
+  } else {
+    return t;
+  }
+}
+
+template <typename F, typename G, typename H>
+auto concatt(F f, G g, H h)
+{
+return [=](auto ... params) {
+return f( g( h( params... ) ) );
+};
+}
+
+int main() {
+  auto twice([](int i) { return i * 2; });
+  auto thrice([](int i) { return i * 3; });
+  auto combined(concat(twice, thrice, plus<int>{}));
+  auto combinedt(concatt(twice, thrice, plus<int>{}));
+  cout << combined(2, 3) << endl;
+  cout << combinedt(2, 3) << endl;
+}
