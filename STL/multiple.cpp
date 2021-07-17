dfb7132b3030c7d7e3077a8535da3f5e407d3d87
fernal73 <linus.fernandes@gmail.com>
2021-07-17 07:58:54 +0530

    Format

--- STL/multiple.cpp
+++ STL/multiple.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-
+using namespace std;
 static auto multicall(auto... functions) {
-  return [=](auto x) {
-    (void)std::initializer_list<int>{((void)functions(x), 0)...};
-  };
+  return
+      [=](auto x) { (void)initializer_list<int>{((void)functions(x), 0)...}; };
 }
 
 static auto for_each(auto f, auto... xs) {
-  (void)std::initializer_list<int>{((void)f(xs), 0)...};
+  (void)initializer_list<int>{((void)f(xs), 0)...};
 }
 
 static auto brace_print(char a, char b) {
-  return [=](auto x) { std::cout << a << x << b << ", "; };
+  return [=](auto x) { cout << a << x << b << ", "; };
 }
 
 int main() {
   auto f(brace_print('(', ')'));
   auto g(brace_print('[', ']'));
   auto h(brace_print('{', '}'));
-  auto nl([](auto) { std::cout << '\n'; });
+  auto nl([](auto) { cout << endl; });
   auto call_fgh(multicall(f, g, h, nl));
   for_each(call_fgh, 1, 2, 3, 4, 5);
 }
