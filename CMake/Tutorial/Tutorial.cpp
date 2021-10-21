bae6d2ca056008a7c8c8ef3dfd37bf951277180a
fernal73 <linus.fernandes@gmail.com>
2021-10-21 15:43:55 +0530

    Add cmake tutorial

---
+++ CMake/Tutorial/Tutorial.cpp
@@ -0,0 +1,14 @@
+/*
+ * Tutorial.cpp
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, world!" << endl;
+}
+
+
