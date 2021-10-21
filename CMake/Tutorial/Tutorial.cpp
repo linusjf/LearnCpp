ddcb69c1383066f0f90bdaaafec89568c1d0855a
fernal73 <linus.fernandes@gmail.com>
2021-10-21 17:26:37 +0530

    Configure further

--- CMake/Tutorial/Tutorial.cpp
+++ CMake/Tutorial/Tutorial.cpp
@@ -5,10 +5,12 @@
  * Distributed under terms of the MIT license.
  */
 #include <iostream>
+#include "TutorialConfig.h"
 using namespace std;
 
-int main(void) {
+int main(const int argc,const char* argv[]) {
+  cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+            << Tutorial_VERSION_MINOR << std::endl;
+  cout << "Usage: " << argv[0] << endl;
   cout << "Hello, world!" << endl;
 }
-
-
