fc812fa7caeab1300d81be1b3fdd83ee82543384
fernal73 <linus.fernandes@gmail.com>
2021-10-22 16:34:27 +0530

    Add Make

---
+++ HelloWorld/PrintNums.cpp
@@ -0,0 +1,15 @@
+/*
+ * PrintNums.cpp
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <iostream>
+using namespace std;
+
+int main(int /*argc*/,const char** /*argv*/) {
+for (int i = 1; i < 11; i++)
+  cout << i << endl;
+}
+
+
