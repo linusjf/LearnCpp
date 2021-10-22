19a1a4a900ec6063fb508cd502755465978f56e6
fernal73 <linus.fernandes@gmail.com>
2021-10-22 05:49:35 +0530

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+/*
+ * HelloWorld.cpp
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <iostream>
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
+
+
